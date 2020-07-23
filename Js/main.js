fa49f89c897cf2c9aa2ca2a2a7595b89b5833ba9
Clinton <57812399+clinton9ice@users.noreply.github.com>
2020-07-23 11:41:09 -0700

    Update

--- Js/main.js
+++ Js/main.js
@@ -1,30 +1,46 @@
 let sections = document.querySelectorAll(".fields");
-let inputs = document.querySelectorAll(".v > .inputs > input");
-let next = document.querySelector("#next")
 
 let index = 0;
-next.onclick = ()=>{
-  if(index == sections.length-1){
-    document.querySelector(".button #next").style.display = "none";
-    document.querySelector(".button button").style.display = "block"
-    return index
-  } else{
-index++
-  }
-
-        
-console.log(index)
-slide(sections)
+$(document).ready(()=>{
+$("#next").on({
+  mouseover: ()=>{
+if($(".v input").val() == ""){
+  $("#next").css({
+    "cursor": "not-allowed",
+    "pointer-event": "none"
+  })
+}else{
+  $("#next").css({
+    "cursor": "pointer",
+  })
 }
-// left.onclick = ()=>{
-//   if(index == 0){
-//     index = carousel.length-1
-//    }else{
-// index--
-//    }
-//    slide(carousel)
-//  slide(id)
-// }
+  },
+  click: ()=>{
+    if($(".v input").val() == ""){
+      $(".inputs").css("border-color","#F73617")
+      $(".err").css({
+        "border-color": "#F55A42",
+        "display": "block"
+      })
+      $(".err").html("Some Fields aren't filled yet")
+    }else{
+      if(index == sections.length-1){
+        $(".button #next").css("display", "none")
+        $(".button button").css("display", "block")
+        return index
+      } else{
+    index++
+      }
+      $(".err").css({
+        "display": "none"
+      })
+      $(".inputs").css("border-color","initial")
+    }
+ 
+slide(sections)
+  }
+})
+})
 function slide(l){
   for(var i = 0; i < l.length; i++){
       l[i].classList.remove('v');
